f9cb6a547045d2af0915ad66ea3a40ee4f7f5328
sak <saknarak@gmail.com>
2023-05-11 16:25:50 +0700

    add upload photo

--- pages/student/[code].vue
+++ pages/student/[code].vue
@@ -1,9 +1,19 @@
 <template>
   <h1>STUDENT {{ $route.params.code }}</h1>
   <hr>
+  <div>
+    <input ref="photo" type="file" class="form-control" multiple>
+  </div>
+  <div>
+    <button class="btn btn-primary" @click="upload">
+      UPLOAD
+    </button>
+  </div>
 </template>
 
 <script>
+import axios from '@/lib/axios'
+
 // OPTION API
 export default {
   data() {
@@ -11,6 +21,16 @@ export default {
     }
   },
   computed: {
+  },
+  methods: {
+    async upload() {
+      const formData = new FormData()
+      formData.append('code', this.$route.params.code)
+      for (let i = 0; i < this.$refs.photo.files.length; i++) {
+        formData.append('photo' + i, this.$refs.photo.files[i])
+      }
+      await axios.post('/api/students/photo', formData)
+    }, // upload
   },
 }
 </script>

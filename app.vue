aad01153a6171890c0280e3b45eb07f2ad2bcf08
sak <saknarak@gmail.com>
2023-05-12 17:24:36 +0700

    add mqtt client

--- app.vue
+++ app.vue
@@ -8,6 +8,9 @@
 export default {
   setup() {
     useHead({
+      script: [
+        { src: '/mqtt.min.js' },
+      ],
       link: [
         {
           href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css',
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <h1>INDEX PAGE</h1>
+  <button @click="doConnect">
+    Connect
+  </button>
   <div :title="`This is ${name}`">
-    My id=<b>{{ id * 10 }}</b>
-    name=<i>{{ name.toUpperCase() }}</i>
+    <div>CODE: {{ code }}</div>
+    <div>TEMP: {{ temp }}</div>
+    <div>HUMI: {{ humi }}</div>
     <div>
       <input type="text" :disabled="true">
     </div>
@@ -15,10 +19,40 @@
 export default {
   data() {
     return {
-      id: 1,
-      name: 'Somsak',
-      age: 25,
+      code: 'unknown',
+      temp: 25,
+      humi: 50,
     }
+  },
+
+  beforeMount() {
+    this.client?.end()
+  },
+
+  methods: {
+    doConnect() {
+      console.log('conn')
+      // mqtt 1883
+      // ws, wss
+      this.client = mqtt.connect('ws://localhost:8083')
+      console.log('....')
+
+      this.client.on('connect', () => {
+        console.log('connected....')
+        this.client.subscribe('demo/+')
+        this.client.subscribe('test/+')
+      })
+
+      this.client.on('message', (topic, message) => {
+        // message is Buffer
+        console.log(topic, message.toString())
+        let data = JSON.parse(message.toString())
+        this.temp = data.temp
+        this.humi = data.humi
+        // topic==demo/01
+        this.code = topic.split('/').pop()
+      })
+    },
   },
 }
 </script>
